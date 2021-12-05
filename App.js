63c101fa5760a052c4252ac847227a87c2c77664
joseph-malak <jmalaks247@gmail.com>
2021-12-04 19:43:15 -0500

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Components/Navbar';
 import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <HomePage />
     </div>
   );
--- Pages/HomePage/index.js
+++ Pages/HomePage/index.js
@@ -2,12 +2,27 @@ import React from 'react'
 import Navbar from '../../Components/Navbar'
 import RegistrationPage from '../RegistrationPage';
 import LoginPage from '../LoginPage';
+import "./style.css";
+import logo from "../../Homepage.png"
 
 const HomePage = () => {
+
+
+
+
+<Navbar />
     return (
+        
         <div>
-            <Navbar />
-            <RegistrationPage />
+            <header>
+            <img src={logo} alt="oop" height = "600px" width = "50%"/>
+            </header>
+            <body>
+           <h1 className = "title">Our Mission:</h1>
+           <h3 className = "discriptionHomePage">Meditation application for the user to gain some quiet time for the mind. The user stops to take some time out during the day to  reconnect with their true self. Choose from a wide selection of meditation styles and track your sessions with our built in timers. We look forward to 
+           helping you Reconnect to the things that matter to you.
+           </h3>
+           </body>
         </div>
     )
 }
