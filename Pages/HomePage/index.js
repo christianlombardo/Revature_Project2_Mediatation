0d0528270061ae7e15ae9d6c3fde03d39686b15c
Christian Lombardo <christianlombardo@verizon.net>
2021-12-04 20:17:21 -0500

    Update.

--- Pages/HomePage/index.js
+++ Pages/HomePage/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Navbar from '../../Components/Navbar'
 import RegistrationPage from '../RegistrationPage';
 import LoginPage from '../LoginPage';
+import MeditationListPage from '../MeditationListPage';
 
 const HomePage = () => {
     return (
         <div>
             <Navbar />
             <RegistrationPage />
+            <MeditationListPage />
         </div>
     )
 }
 
 export default HomePage;
-
