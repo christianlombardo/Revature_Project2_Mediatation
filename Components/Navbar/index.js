2ffda8f5528479f7cd083c9a82fb4fd2f3eec74e
joseph-malak <jmalaks247@gmail.com>
2021-12-13 01:04:14 -0500

    first commit

--- Components/Navbar/index.js
+++ Components/Navbar/index.js
@@ -5,16 +5,29 @@ import {Link} from 'react-router-dom';
 import LoggedNavbar from './LoggedNavbar';
 import NotLoggedNavbar from './NotLoggedNavbar';
 
-const Navbar = (props) => {
-if(props.isLoggedIn===true){
+const Navbar = () => {
+//if(props.isLoggedIn===true){
 
-    return <LoggedNavbar />
-}
-return <NotLoggedNavbar />
+   // return <LoggedNavbar />
+//}
+//return <NotLoggedNavbar />
     
+//}
+return(
+<div className = "navWrapper">
+      <nav className="navbar navbar-expand-lg navbar-light">
+  <div className="container-fluid">
+    <a className="navbar-brand">Reconnect</a>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-link active" aria-current="page" to = '/HomePage'>Home</Link>
+        <Link className="nav-link active" aria-current="page" to = '/RegistrationPage'>Create an account</Link>
+        <Link className="nav-link active" aria-current="page" to = '/LoginPage'>Login</Link>
+      </div>
+    </div>
+  </div>
+</nav>
+</div>
+)
 }
-
 export default Navbar;
-
-
- 
