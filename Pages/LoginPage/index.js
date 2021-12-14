a097b7f0579d833e4b0b82d525dc84a63255e67d
Christian Lombardo <christianlombardo@verizon.net>
2021-12-13 23:06:03 -0500

    Registration page functioning.

--- Pages/LoginPage/index.js
+++ Pages/LoginPage/index.js
@@ -30,41 +30,41 @@ function onChangeHandler(event){
 
     return ( 
 
-<form onSubmit = {submitHandler}>
-<div className = "registrationWrapper ">  
-    <section className="vh-100 gradient-custom">
-        <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-                <div className="col-12 col-md-9 col-lg-5 col-xl-5">
-                    <div className="card bg-primary text-white">
-                        <div className="card-body p-5 text-center">
+        <form onSubmit = {submitHandler}>
+            <div className = "registrationWrapper ">  
+                <section className="vh-100 gradient-custom">
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-12 col-md-9 col-lg-5 col-xl-5">
+                                <div className="card bg-primary text-white">
+                                    <div className="card-body p-5 text-center">
 
-                            <div className="mb-md-5 mt-md-4 pb-5">
-                                {/*{(error != "") ? (<div className="error">{error}</div>) :""}*/}
-                                <h2 className="fw-bold mb-2 text">Login Form</h2>
-                                <p className="text-white-50 mb-5">Please enter your email and password</p>
+                                        <div className="mb-md-5 mt-md-4 pb-5">
+                                            {/*{(error != "") ? (<div className="error">{error}</div>) :""}*/}
+                                            <h2 className="fw-bold mb-2 text">Login Form</h2>
+                                            <p className="text-white-50 mb-5">Please enter your email and password</p>
 
-                                <div className="form-outline form-white mb-4">
-                                    <input type="text" id="username" className="form-control form-control-lg" name="username" onChange = {onChangeHandler} value ={details.name}/>
-                                    <label className="form-label" for="username" >Email</label>
+                                            <div className="form-outline form-white mb-4">
+                                                <input type="text" id="username" className="form-control form-control-lg" name="username" onChange = {onChangeHandler} value ={details.name}/>
+                                                <label className="form-label" for="username" >Email</label>
+                                            </div>
+
+                                            <div className="form-outline form-white mb-4">
+                                                <input type="password" id="password" className="form-control form-control-lg" name="password" onChange = {onChangeHandler} value ={details.password}/>
+                                                <label className="form-label" for="password">Password</label>
+                                            </div>
+
+                                            <button className="btn btn-outline-primary btn-lg px-10" type="submit" value = "login">Login</button>
+
+                                    </div>
                                 </div>
-
-                                <div className="form-outline form-white mb-4">
-                                    <input type="password" id="password" className="form-control form-control-lg" name="password" onChange = {onChangeHandler} value ={details.password}/>
-                                    <label className="form-label" for="password">Password</label>
-                                </div>
-
-                                <button className="btn btn-outline-primary btn-lg px-10" type="submit" value = "login">Login</button>
-
                             </div>
                         </div>
                     </div>
                 </div>
+            </section>
             </div>
-        </div>
-    </section>
-    </div>
-</form>
+        </form>
 
 )
 }
