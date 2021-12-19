59633d5b26c1bd61fc91bd23689869875d8b9a49
Christian Lombardo <christianlombardo@verizon.net>
2021-12-19 18:46:26 -0500

    MeditatingBreathePage update.

--- Pages/MeditatingPage/MeditatingBreathePage.js
+++ Pages/MeditatingPage/MeditatingBreathePage.js
@@ -56,68 +56,69 @@ const MeditatingBreathePage = (props) => {
             console.log(response);
          
         });
-        submittingBreathe.sending = true;
+        setSubmitting(true)
     };
 
-    if(submittingBreathe.sending == false){
+
+    if(submittingBreathe.sending === false){
    
-    return(
-       
-        <div className = "wrapperpages">
-            {clearState()}
-          <NavbarLogged />
-            <div className="container text-center">
-                <h1>{/*props.name*/}</h1> { /* <FavoriteButton /> */}
-                {/* <TimerDisplay />
-                <TimerButtons /> */}
-                
-                <TimerClass minutes="15" /> {/*minutes=props.minutes*/}
-                {/*meditation={props.meditation_id}*/}
-                {
-                // Dynamic Instructions
-                    // Every minute give a new set of instructions.
-                    // 5 minutes - 5 instructions.
-                    
-                }
-                {/* { props.instructions.map((instruction) => ) } */}
-
-                <h1>Instructions for Breathing Style:</h1>
-                <ul class="list-group">
-                <li class="list-group-item">1. Gently breathe deeply in air into your nose ...</li>
-                <li class="list-group-item">2. feel your abdomen gently expand...</li>
-                <li class="list-group-item">3. breathe out of your mouth and let it all go.</li>
-                <li class="list-group-item">4. Gently repeat.</li>
-                </ul>
-
-                <form onSubmit = {submitHandler}>
-                    <br></br>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Please write your experience and feelings about today's session</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1"  name="journalnotes" onChange={onChangeHandler} value={journal.journalnotes} rows="3"></textarea>
-                    </div>
-                    <button className="btn btn-outline-primary btn-lg px-10" type="submit" onSubmit={submitHandler} >Submit</button>
-                </form>
-
-                <br></br>
-                <Link to= '/MeditationList' className="lastLink">Go to List of Meditations</Link>
-                {/* Diary Notes textarea to display after the meditation timer completes.*/}
-
-                {/* 
-                <MeditationInstructions />
-                <MeditationDiaryNotes /> */}
-            </div>
-        </div>
-        )
-    }
-    else{
         return(
-           <div>
-               
-            <MeditationListPage />
-         
+        
+            <div className = "wrapperpages">
+                {clearState()}
+            <NavbarLogged />
+                <div className="container text-center">
+                    <h1>{/*props.name*/}</h1> { /* <FavoriteButton /> */}
+                    {/* <TimerDisplay />
+                    <TimerButtons /> */}
+                    
+                    <TimerClass minutes="15" /> {/*minutes=props.minutes*/}
+                    {/*meditation={props.meditation_id}*/}
+                    {
+                    // Dynamic Instructions
+                        // Every minute give a new set of instructions.
+                        // 5 minutes - 5 instructions.
+                        
+                    }
+                    {/* { props.instructions.map((instruction) => ) } */}
+
+                    <h1>Instructions for Breathing Style:</h1>
+                    <ul class="list-group">
+                    <li class="list-group-item">1. Gently breathe deeply in air into your nose ...</li>
+                    <li class="list-group-item">2. feel your abdomen gently expand...</li>
+                    <li class="list-group-item">3. breathe out of your mouth and let it all go.</li>
+                    <li class="list-group-item">4. Gently repeat.</li>
+                    </ul>
+
+                    <form onSubmit = {submitHandler}>
+                        <br></br>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Please write your experience and feelings about today's session</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1"  name="journalnotes" onChange={onChangeHandler} value={journal.journalnotes} rows="3"></textarea>
+                        </div>
+                        <button className="btn btn-outline-primary btn-lg px-10" type="submit" onSubmit={submitHandler} >Submit</button>
+                    </form>
+
+                    <br></br>
+                    <Link to= '/MeditationList' className="lastLink">Go to List of Meditations</Link>
+                    {/* Diary Notes textarea to display after the meditation timer completes.*/}
+
+                    {/* 
+                    <MeditationInstructions />
+                    <MeditationDiaryNotes /> */}
+                </div>
             </div>
-        )
-    }
+            )
+        }
+        else{
+            return(
+            <div>
+                
+                <MeditationListPage />
+            
+                </div>
+            )
+        }
 }
 
 
